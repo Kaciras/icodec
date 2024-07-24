40788038897d1de1bfce3ea2b7bb3951dd9b4748
Kaciras <Kaciras@protonmail.com>
2024-07-24 18:33:51 +0800

    export build functions

--- build.js
+++ build.js
@@ -2,119 +2,29 @@ import { execFileSync, execSync } from "child_process";
 import { existsSync, mkdirSync, renameSync } from "fs";
 import { join } from "path";
 
-// Must build WebP before to generate libsharpyuv.a
-async function buildAVIF() {
-	gitClone("vendor/libavif","v1.1.0", "https://github.com/AOMediaCodec/libavif");
-	gitClone("vendor/libavif/ext/aom","v3.9.1", "https://aomedia.googlesource.com/aom");
+function detectVisualStudio() {
+	const pf32 = process.env["ProgramFiles(x86)"];
+	if (!pf32) {
+		return;
+	}
+	const vswhere = `${pf32}/Microsoft Visual Studio/Installer/vswhere.exe`;
+	const stdout = execFileSync(vswhere, { encoding: "utf8" });
+	const properties = Object.fromEntries(stdout.split("\r\n").map(line => line.split(": ")));
 
-	mkdirSync("vendor/libavif/ext/aom/build.libavif", { recursive: true });
-	cmake(
-		"vendor/libavif/ext/aom/build.libavif/libaom.a",
-		"vendor/libavif/ext/aom",
-		"vendor/libavif/ext/aom/build.libavif",
-		{
-			CMAKE_BUILD_TYPE: "Release",
-			ENABLE_CCACHE: "0",
-			AOM_TARGET_CPU: "generic",
-			AOM_EXTRA_C_FLAGS: "-UNDEBUG",
-			AOM_EXTRA_CXX_FLAGS: "-UNDEBUG",
-			ENABLE_DOCS: "0",
-			ENABLE_TESTS: "0",
-			ENABLE_EXAMPLES: "0",
-			ENABLE_TOOLS: "0",
-			CONFIG_ACCOUNTING: "1",
-			CONFIG_INSPECTION: "0",
-			CONFIG_RUNTIME_CPU_DETECT: "0",
-			CONFIG_WEBM_IO: "0",
-
-			CONFIG_MULTITHREAD: "0",
-			CONFIG_AV1_HIGHBITDEPTH: "0",
-		},
-	);
-
-	cmake("vendor/libavif/libavif.a","vendor/libavif","vendor/libavif", {
-		// "-DCMAKE_BUILD_TYPE=Release",
-		// "-DAOM_LIBRARY=vendor/ext/aom/build.libavif/libaom.a",
-		// "-DAOM_INCLUDE_DIR=../libaom",
-		BUILD_SHARED_LIBS: "0",
-		AVIF_CODEC_AOM: "LOCAL",
-		AVIF_LOCAL_LIBSHARPYUV: "1",
-		LIBYUV_LIBRARY: "../libwebp/libsharpyuv.a",
-		LIBYUV_INCLUDE_DIR: "../libwebp/sharpyuv",
-	});
-
-	// emcc([
-	// 	"-I vendor/libavif/include",
-	// 	"-o lib/avif-enc.js",
-	//
-	// 	"src/avif_enc.cpp",
-	// 	"vendor/libavif/libavif.a",
-	// 	"vendor/libavif/ext/aom/build.libavif/libaom.a",
-	// ]);
-
-	emcc([
-		"-I vendor/libavif/include",
-		"-o dist/avif-dec.js",
-
-		"cpp/avif_dec.cpp",
-		"vendor/libavif/libavif.a",
-		"vendor/libavif/ext/aom/build.libavif/libaom.a",
-	]);
+	const path = properties.resolvedInstallationPath;
+	return {
+		version: parseInt(properties.installationVersion),
+		path,
+		clang: path + "/VC/Tools/Llvm/x64/bin",
+		productVersion: properties.catalog_productLineVersion,
+	};
 }
 
-async function buildWebP2() {
-	await downloadSource("libwebp2", "https://chromium.googlesource.com/codecs/libwebp2/+archive/9dd38de9c8905af1eab0914fd40531e970b309c5.tar.gz", 0);
-	mkdirSync("vendor/wp2_build", { recursive: true });
+let clangDirectory;
 
-	const args1 = [
-		"emcmake", "cmake", "-S ../libwebp2", "-B .",
-
-		"-DWP2_BUILD_TESTS=0",
-		"-DWP2_ENABLE_TESTS=0",
-		"-DWP2_BUILD_EXAMPLES=0",
-		"-DWP2_BUILD_EXTRAS=0",
-		"-DWP2_REDUCED=1",
-
-		"-DCMAKE_DISABLE_FIND_PACKAGE_Threads=1",
-		"-DWP2_ENABLE_SIMD=1",
-	];
-	execSync(args1.join(" "), { cwd: "vendor/wp2_build", stdio: "inherit" });
-	execSync("cmake --build .", { cwd: "vendor/wp2_build", stdio: "inherit" });
-
-	const args2 = [
-		"emcc",
-		"-O3",
-		"--bind",
-		"-s ALLOW_MEMORY_GROWTH=1",
-		"-s ENVIRONMENT=web",
-		"-s EXPORT_ES6=1",
-
-		"-I vendor/libwebp2/src/wp2",
-		"-o lib/wp2-enc.js",
-
-		"src/wp2_enc.cpp",
-		"vendor/wp2_build/libwebp2.a",
-	];
-
-	execSync(args2.join(" "), { stdio: "inherit" });
-}
-
-function buildMozJPEG() {
-	gitClone("vendor/mozjpeg","v4.1.5","https://github.com/mozilla/mozjpeg");
-	cmake("vendor/mozjpeg/libjpeg.a", "vendor/mozjpeg", "vendor/mozjpeg", {
-		WITH_SIMD: "0",
-		ENABLE_SHARED: "0",
-		WITH_TURBOJPEG: "0",
-		PNG_SUPPORTED: "0",
-	});
-	emcc([
-		"-I vendor/mozjpeg",
-		"-o dist/mozjpeg-enc.js",
-
-		"cpp/mozjpeg_enc.cpp",
-		"vendor/mozjpeg/libjpeg.a",
-		"vendor/mozjpeg/rdswitch.c",
-	]);
+if (process.platform === "win32") {
+	const vs = detectVisualStudio();
+	clangDirectory = vs.clang;
 }
 
 let cmakeBuilder = null;
@@ -153,12 +63,52 @@ function cmake(checkFile, src, dist, options) {
 	execSync("cmake --build .", { cwd: dist, stdio: "inherit" });
 }
 
-function buildQOI() {
+export function buildMozJPEG() {
+	gitClone("vendor/mozjpeg", "v4.1.5", "https://github.com/mozilla/mozjpeg");
+	cmake("vendor/mozjpeg/libjpeg.a", "vendor/mozjpeg", "vendor/mozjpeg", {
+		WITH_SIMD: "0",
+		ENABLE_SHARED: "0",
+		WITH_TURBOJPEG: "0",
+		PNG_SUPPORTED: "0",
+	});
+	emcc([
+		"-I vendor/mozjpeg",
+		"-o dist/mozjpeg-enc.js",
+
+		"cpp/mozjpeg_enc.cpp",
+		"vendor/mozjpeg/libjpeg.a",
+		"vendor/mozjpeg/rdswitch.c",
+	]);
+}
+
+export function buildPNGQuant() {
+	const env = { ...process.env };
+	if (env.EMSDK) {
+		env.CC = join(env.EMSDK, "upstream/bin/clang");
+	}
+
+	// https://github.com/rustwasm/wasm-pack/blob/62ab39cf82ec4d358c1f08f348cd0dc44768f412/src/command/build.rs#L116
+	const args = [
+		"build", "rust",
+		// "--dev",
+		"--no-typescript",
+		"--no-pack",
+		"--reference-types",
+		"--weak-refs",
+		"--target", "web",
+	];
+	execFileSync("wasm-pack", args, { stdio: "inherit", env });
+
+	renameSync("rust/pkg/pngquant.js", "dist/pngquant.js");
+	renameSync("rust/pkg/pngquant_bg.wasm", "dist/pngquant_bg.wasm");
+}
+
+export function buildQOI() {
 	gitClone("vendor/qoi", "master", "https://github.com/phoboslab/qoi");
 	emcc(["-I vendor/qoi", "-o dist/qoi.js", "cpp/qoi.cpp"]);
 }
 
-async function buildWebpEncoder() {
+export function buildWebP() {
 	gitClone("vendor/libwebp", "v1.4.0", "https://github.com/webmproject/libwebp");
 	cmake("vendor/libwebp/libwebp.a", "vendor/libwebp", "vendor/libwebp", {
 		WEBP_ENABLE_SIMD: "1",
@@ -183,9 +133,17 @@ async function buildWebpEncoder() {
 		"vendor/libwebp/libwebp.a",
 		"vendor/libwebp/libsharpyuv.a",
 	]);
+
+	emcc([
+		"-o dist/webp-dec.js",
+		"-I vendor/libwebp",
+		"cpp/webp_dec.cpp",
+		"vendor/libwebp/libwebp.a",
+		"vendor/libwebp/libsharpyuv.a",
+	]);
 }
 
-function buildJXLEncoder() {
+export function buildJXL() {
 	gitClone("libjxl", "v0.8.3", "https://github.com/libjxl/libjxl");
 	cmake("vendor/libjxl/lib/libjxl.a", "vendor/libjxl", "vendor/libjxl", {
 		BUILD_SHARED_LIBS: "0",
@@ -215,57 +173,101 @@ function buildJXLEncoder() {
 	]);
 }
 
-function detectVisualStudio() {
-	const pf32 = process.env["ProgramFiles(x86)"];
-	if (!pf32) {
-		return;
-	}
-	const vswhere = `${pf32}/Microsoft Visual Studio/Installer/vswhere.exe`;
-	const stdout = execFileSync(vswhere, { encoding: "utf8" });
-	const properties = Object.fromEntries(stdout.split("\r\n").map(line => line.split(": ")));
+// Must build WebP before to generate libsharpyuv.a
+export function buildAVIF() {
+	gitClone("vendor/libavif", "v1.1.0", "https://github.com/AOMediaCodec/libavif");
+	gitClone("vendor/libavif/ext/aom", "v3.9.1", "https://aomedia.googlesource.com/aom");
 
-	const path = properties.resolvedInstallationPath;
-	return {
-		version: parseInt(properties.installationVersion),
-		path,
-		clang: path + "/VC/Tools/Llvm/x64/bin",
-		productVersion: properties.catalog_productLineVersion,
-	};
+	mkdirSync("vendor/libavif/ext/aom/build.libavif", { recursive: true });
+	cmake(
+		"vendor/libavif/ext/aom/build.libavif/libaom.a",
+		"vendor/libavif/ext/aom",
+		"vendor/libavif/ext/aom/build.libavif",
+		{
+			CMAKE_BUILD_TYPE: "Release",
+			ENABLE_CCACHE: "0",
+			AOM_TARGET_CPU: "generic",
+			AOM_EXTRA_C_FLAGS: "-UNDEBUG",
+			AOM_EXTRA_CXX_FLAGS: "-UNDEBUG",
+			ENABLE_DOCS: "0",
+			ENABLE_TESTS: "0",
+			ENABLE_EXAMPLES: "0",
+			ENABLE_TOOLS: "0",
+			CONFIG_ACCOUNTING: "1",
+			CONFIG_INSPECTION: "0",
+			CONFIG_RUNTIME_CPU_DETECT: "0",
+			CONFIG_WEBM_IO: "0",
+
+			CONFIG_MULTITHREAD: "0",
+			CONFIG_AV1_HIGHBITDEPTH: "0",
+		},
+	);
+
+	cmake("vendor/libavif/libavif.a", "vendor/libavif", "vendor/libavif", {
+		// "-DCMAKE_BUILD_TYPE=Release",
+		// "-DAOM_LIBRARY=vendor/ext/aom/build.libavif/libaom.a",
+		// "-DAOM_INCLUDE_DIR=../libaom",
+		BUILD_SHARED_LIBS: "0",
+		AVIF_CODEC_AOM: "LOCAL",
+		AVIF_LOCAL_LIBSHARPYUV: "1",
+		LIBYUV_LIBRARY: "../libwebp/libsharpyuv.a",
+		LIBYUV_INCLUDE_DIR: "../libwebp/sharpyuv",
+	});
+
+	emcc([
+		"-I vendor/libavif/include",
+		"-o lib/avif-enc.js",
+
+		"src/avif_enc.cpp",
+		"vendor/libavif/libavif.a",
+		"vendor/libavif/ext/aom/build.libavif/libaom.a",
+	]);
+
+	emcc([
+		"-I vendor/libavif/include",
+		"-o dist/avif-dec.js",
+
+		"cpp/avif_dec.cpp",
+		"vendor/libavif/libavif.a",
+		"vendor/libavif/ext/aom/build.libavif/libaom.a",
+	]);
 }
 
-let clangDirectory;
+export function buildWebP2() {
+	gitClone(
+		"vendor/libwebp2",
+		"main",
+		"https://chromium.googlesource.com/codecs/libwebp2",
+	);
 
-if (process.platform === "win32") {
-	const vs = detectVisualStudio();
-	clangDirectory = vs.clang;
+	mkdirSync("vendor/wp2_build", { recursive: true });
+
+	cmake("vendor/wp2_build/libwebp2.a", "vendor/libwebp2", "vendor/wp2_build", {
+		WP2_BUILD_TESTS: "0",
+		WP2_ENABLE_TESTS: "0",
+		WP2_BUILD_EXAMPLES: "0",
+		WP2_BUILD_EXTRAS: "0",
+		WP2_REDUCED: "1",
+		CMAKE_DISABLE_FIND_PACKAGE_Threads: "1",
+		WP2_ENABLE_SIMD: "1",
+	});
+
+	emcc([
+		"-I vendor/libwebp2/src/wp2",
+		"-o lib/wp2-enc.js",
+
+		"src/wp2_enc.cpp",
+		"vendor/wp2_build/libwebp2.a",
+	]);
 }
 
-// cmake("vendor/mozjpeg", mozjpeg, true);
-// cmake("vendor/libjxl", libjxl);
-// cmake("vendor/qoi", qoi);
-// buildWebpEncoder();
-
-function buildPNGQuant() {
-	const env = { ...process.env };
-	if (env.EMSDK) {
-		env.CC = join(env.EMSDK, "upstream/bin/clang");
-	}
-
-	// https://github.com/rustwasm/wasm-pack/blob/62ab39cf82ec4d358c1f08f348cd0dc44768f412/src/command/build.rs#L116
-	const args = [
-		"build", "rust",
-		// "--dev",
-		"--no-typescript",
-		"--no-pack",
-		"--reference-types",
-		"--weak-refs",
-		"--target", "web",
-	];
-	execFileSync("wasm-pack", args, { stdio: "inherit", env });
-
-	renameSync("rust/pkg/pngquant.js", "dist/pngquant.js");
-	renameSync("rust/pkg/pngquant_bg.wasm", "dist/pngquant_bg.wasm");
+// Equivalent to `if __name__ == "__main__":` in Python.
+if (process.argv[1] === import.meta.filename) {
+	buildWebP();
+	buildAVIF();
+	buildWebP2();
+	buildJXL();
+	buildQOI();
+	buildPNGQuant();
+	buildMozJPEG();
 }
-
-// buildMozJPEG();
-buildAVIF();
