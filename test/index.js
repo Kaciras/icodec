c79a5f6b28392ed94cd30bbe2139e19a73fe8a68
Kaciras <Kaciras@protonmail.com>
2024-07-24 18:39:56 +0800

    add image snapshots to git

--- test/index.js
+++ test/index.js
@@ -1,21 +1,19 @@
-import { mkdirSync, readFileSync, writeFileSync } from "fs";
-import { test } from "node:test";
+import { readFileSync, writeFileSync } from "fs";
+import { describe, test } from "node:test";
 import * as assert from "assert";
 import { join } from "path";
 import pixelMatch from "pixelmatch";
 import * as jpeg from "../lib/jpeg.js";
 import * as png from "../lib/png.js";
+import * as qoi from "../lib/qoi.js";
 import * as webp from "../lib/webp.js";
 import * as avif from "../lib/avif.js";
-import * as qoi from "../lib/qoi.js";
 
-const rawBuffer = readFileSync("test/image.bin");
+const rawBuffer = readFileSync("test/snapshot/image.bin");
 const snapshotDirectory = "test/snapshot";
 
 const w = 417;
 const h = 114;
-
-mkdirSync(snapshotDirectory, { recursive: true });
 
 function assertImageEquals(buffer1, buffer2) {
 	assert.strictEqual(buffer1.byteLength, buffer2.byteLength);
@@ -23,67 +21,72 @@ function assertImageEquals(buffer1, buffer2) {
 	pixelMatch(buffer1, buffer2, diff, w, h, { threshold: 0.1 });
 }
 
-test("JPEG encode", async () => {
-	await jpeg.initialize();
+describe("Encode", () => {
+	test("JPEG", async () => {
+		await jpeg.initialize();
 
-	const encoded = jpeg.encode(rawBuffer, 417, 114, {});
+		const encoded = jpeg.encode(rawBuffer, 417, 114, {});
 
-	writeFileSync(join(snapshotDirectory, "image.jpeg"), encoded);
-	assert.ok(encoded.length < 7 * 1024);
+		writeFileSync(join(snapshotDirectory, "image.jpeg"), encoded);
+		assert.ok(encoded.length < 7 * 1024);
+	});
+
+	test("PNG", async () => {
+		await png.initialize();
+		const encoded = png.optimize(rawBuffer, 417, 114, {});
+		writeFileSync(join(snapshotDirectory, "image.png"), encoded);
+		assert.ok(encoded.length < 7 * 1024);
+	});
+
+	test("WebP", async () => {
+		await webp.initialize();
+
+		const encoded = webp.encode(rawBuffer, 417, 114, {});
+
+		writeFileSync(join(snapshotDirectory, "image.webp"), encoded);
+		assert.ok(encoded.length < 7 * 1024);
+	});
+
+
+	test("AVIF", async () => {
+		await avif.initialize();
+
+		const encoded = avif.encode(rawBuffer, 417, 114, {});
+
+		writeFileSync(join(snapshotDirectory, "image.avif"), encoded);
+		assert.ok(encoded.length < 7 * 1024);
+	});
+
+	test("QOI", async () => {
+		await qoi.initialize();
+
+		const encoded = qoi.encode(rawBuffer, 417, 114, {});
+
+		writeFileSync(join(snapshotDirectory, "image.qoi"), encoded);
+		assert.ok(encoded.length < 18 * 1024);
+	});
 });
 
-test("PNG encode", async () => {
-	await png.initialize();
-	const encoded = png.optimize(rawBuffer, 417, 114, {});
-	writeFileSync(join(snapshotDirectory, "image.png"), encoded);
-	assert.ok(encoded.length < 7 * 1024);
-});
+describe("Decode", () => {
+	test("WebP", async () => {
+		await webp.initialize();
 
-test("WebP encode", async () => {
-	await webp.initialize();
+		const image = webp.decode(readFileSync("test/snapshot/image.webp"));
 
-	const encoded = webp.encode(rawBuffer, 417, 114, {});
+		assertImageEquals(image.data, rawBuffer);
+	});
 
-	writeFileSync(join(snapshotDirectory, "image.webp"), encoded);
-	assert.ok(encoded.length < 7 * 1024);
-});
+	test("AVIF", async () => {
+		await avif.initialize();
 
-test("WebP decode", async () => {
-	await webp.initialize();
+		const image = avif.decode(readFileSync("test/snapshot/image.avif"));
 
-	const image = webp.decode(readFileSync("test/snapshot/image.webp"));
+		assertImageEquals(image.data, rawBuffer);
+	});
 
-	assertImageEquals(image.data, rawBuffer);
-});
-
-test("AVIF encode", async () => {
-	await avif.initialize();
-
-	const encoded = avif.encode(rawBuffer, 417, 114, {});
-
-	writeFileSync(join(snapshotDirectory, "image.avif"), encoded);
-	assert.ok(encoded.length < 7 * 1024);
-});
-
-test("AVIF decode", async () => {
-	await avif.initialize();
-
-	const image = avif.decode(readFileSync("test/snapshot/image.avif"));
-
-	assertImageEquals(image.data, rawBuffer);
-});
-
-test("QOI encode", async () => {
-	await qoi.initialize();
-
-	const encoded = qoi.encode(rawBuffer, 417, 114, {});
-
-	writeFileSync(join(snapshotDirectory, "image.qoi"), encoded);
-	assert.ok(encoded.length < 18 * 1024);
-});
-
-test("QOI decode", async () => {
-	await qoi.initialize();
-	const image = qoi.decode(readFileSync("test/snapshot/image.qoi"));
-	assertImageEquals(image.data, rawBuffer);
+	test("QOI", async () => {
+		await qoi.initialize();
+		const image = qoi.decode(readFileSync("test/snapshot/image.qoi"));
+		assertImageEquals(image.data, rawBuffer);
+	});
 });
