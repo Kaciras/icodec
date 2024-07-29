45bc9dbe9ad5331454aa3ca8314cf9b21850eb47
Kaciras <Kaciras@protonmail.com>
2024-07-29 17:55:44 +0800

    add entry point for Node

--- benchmark/encode.ts
+++ benchmark/encode.ts
@@ -1,13 +1,33 @@
-import { defineSuite } from "esbench";
-import { initialize, optimize } from "../lib/png.ts";
 import { readFileSync } from "fs";
-import sharp from "sharp";
+import sharp, { Sharp, SharpOptions } from "sharp";
+import { defineSuite } from "esbench";
+import * as codecs from "../lib/node.js";
 
-const rawBuffer = readFileSync("test/snapshot/image.bin");
-await initialize();
-const si = sharp(rawBuffer, { raw: { width: 417, height: 114, channels: 4 } });
+const sharpOptions: SharpOptions = { raw: { width: 417, height: 114, channels: 4 } };
+const bytes = readFileSync("test/snapshot/image.bin");
 
-export default defineSuite(scene => {
-	scene.bench("WASM", () => optimize(rawBuffer, 417, 114, {}));
-	scene.benchAsync("Sharp", () => si.png({ quality: 75, palette: true }).toBuffer());
+const sharpEncode: Record<string, (image: Sharp) => Sharp> = {
+	avif: image => image.avif(),
+	jpeg: image => image.jpeg(),
+	png: image => image.png({ quality: 75, palette: true }),
+	jxl: image => image.jxl(),
+	webp: image => image.webp(),
+};
+
+export default defineSuite({
+	params: {
+		codec: ["avif", "jpeg", "png", "webp"],
+	},
+	baseline: {
+		type: "Name",
+		value: "Sharp",
+	},
+	async setup(scene) {
+		const name = scene.params.codec;
+		const { loadEncoder, encode } = codecs[name];
+		await loadEncoder();
+
+		scene.bench("WASM", () => encode(bytes, 417, 114));
+		scene.benchAsync("Sharp", () => sharpEncode[name](sharp(bytes, sharpOptions)).toBuffer());
+	},
 });
