5639f0ad191f85ad906afa7b08716117abdcb782
Kaciras <Kaciras@protonmail.com>
2024-08-09 16:08:55 +0800

    optimize demo page

--- test/demo.js
+++ test/demo.js
@@ -2,6 +2,7 @@ import * as codecs from "../lib/index.js";
 
 const worker = new Worker("./test/worker.js", { type: "module" });
 
+const fileChooser = document.getElementById("file");
 const select = document.querySelector("select");
 const encodeButton = document.getElementById("encode");
 const textarea = document.querySelector("textarea");
@@ -12,7 +13,7 @@ const ctx2D = canvas.getContext("2d");
 
 let sharedImageData;
 
-document.getElementById("file").oninput = async event => {
+fileChooser.oninput = async event => {
 	const [file] = event.currentTarget.files;
 	const codec = getCodec(file);
 	await decode(file, codec);
@@ -62,7 +63,14 @@ async function decode(file, decoder) {
 	sharedImageData = { data: bytes, width, height };
 }
 
-function callWorker(args, transfer) {
+/**
+ * Call encode function in the worker thread, prevents block the UI thread.
+ * Multithreaded encoder is also need to run in Worker.
+ *
+ * @param args {unknown[]} Arguments passed to worker.
+ * @param transfer {Transferable[]} An optional array of transferable objects to transfer ownership of.
+ */
+function invokeInWorker(args, transfer) {
 	worker.postMessage(args, transfer);
 	return new Promise((resolve, reject) => {
 		worker.onerror = reject;
@@ -70,18 +78,19 @@ function callWorker(args, transfer) {
 	});
 }
 
-async function encode(codec) {
+async function encode(codec = select.value) {
 	encodeButton.classList.add("busy");
 
-	const options = textarea.value
-		? JSON.parse(textarea.value)
-		: undefined;
-
-	const output = await callWorker([codec, sharedImageData, options]);
+	const options = textarea.value ? JSON.parse(textarea.value) : undefined;
+	const output = await invokeInWorker([codec, sharedImageData, options]);
 	encodeButton.classList.remove("busy");
 
-	const { extension } = codecs[codec];
-	download(new File([output], "output." + extension));
+	let { name } = fileChooser.files[0];
+	const dot = name.lastIndexOf(".");
+	if (dot !== -1) {
+		name = name.slice(0, dot - 1);
+	}
+	download(new File([output], name + codecs[codec].extension));
 }
 
 function download(file) {
@@ -97,6 +106,7 @@ function refreshOptions() {
 		? JSON.stringify(defaultOptions, null, "\t") : "";
 }
 
-select.oninput = refreshOptions;
-refreshOptions();
-encodeButton.onclick = () => encodeButton.classList.contains("busy") || encode(select.value);
+// Set event handler, and call it immediately.
+(select.oninput = refreshOptions)();
+
+encodeButton.onclick = () => encodeButton.classList.contains("busy") || encode();
